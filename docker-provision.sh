c258475cbfd50b188f02732e30fadfecbf72d4ce
hg-jt <hg-jt@users.noreply.github.com>
2015-12-15 15:24:42 -0500

    Added wrapper script to autoinstall GitBook plugins.

--- docker-provision.sh
+++ docker-provision.sh
@@ -17,6 +17,15 @@ gitbook versions:install latest
 # add commonly used GitBook plugins
 npm install -g gitbook-plugin-include-codeblock
 
+# add gitbook wrapper script
+cat <<EOF > /usr/local/bin/gitbookw
+#!/bin/sh -eu
+# gitbookw --- Wrapper for gitbook that autoinstalls plugins.
+
+gitbook install
+gitbook \$@
+EOF
+chmod +x /usr/local/bin/gitbookw
 
 # clean up (apt)
 apt-get clean     # remove packages that have been downloaded, installed, and no longer needed
